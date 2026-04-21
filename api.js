f2c9e398c537cd25ce9923944e642d4c048579f6
parithym <parithym05@gmail.com>
2026-04-21 09:39:35 +0530

    Update api.js

--- api.js
+++ api.js
@@ -4,17 +4,15 @@ const BASE_URL = "https://t4e-testserver.onrender.com/api";
 
 export const getToken = async () => {
   const response = await axios.post(`${BASE_URL}/public/token`, {
-    studentId: "E0423017", 
-    password: "451560",        
+    studentId: "YOUR_REGISTER_NUMBER",
+    password: "YOUR_PASSWORD",
   });
   return response.data.token;
 };
 
 export const getData = async (token) => {
   const response = await axios.get(`${BASE_URL}/private/data`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
+    headers: { Authorization: `Bearer ${token}` },
   });
   return response.data;
 };
