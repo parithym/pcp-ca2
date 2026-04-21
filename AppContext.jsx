eaed7c795a3c266fc674fa67041c3c9dfb5ec4b4
parithym <parithym05@gmail.com>
2026-04-21 09:21:17 +0530

    Create AppContext.jsx

---
+++ AppContext.jsx
@@ -0,0 +1,33 @@
+import React, { createContext, useReducer, useEffect } from "react";
+import AppReducer from "../reducer/AppReducer";
+import { getToken, getData } from "../services/api";
+
+const initialState = {
+  workouts: [],
+  favorites: [],
+};
+
+export const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const token = await getToken();
+        const data = await getData(token);
+        dispatch({ type: "SET_WORKOUTS", payload: data });
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <AppContext.Provider value={{ state, dispatch }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
