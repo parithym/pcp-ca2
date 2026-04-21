13f57c41117fcf2e0fa75aa7868f0856bb4192f3
parithym <parithym05@gmail.com>
2026-04-21 09:39:09 +0530

    Update AppReducer.js

--- AppReducer.js
+++ AppReducer.js
@@ -1,18 +1,11 @@
 const AppReducer = (state, action) => {
   switch (action.type) {
-
     case "SET_WORKOUTS":
       return { ...state, workouts: action.payload };
-
     case "ADD_WORKOUT":
       return { ...state, workouts: [...state.workouts, action.payload] };
-
     case "DELETE_WORKOUT":
-      return {
-        ...state,
-        workouts: state.workouts.filter((w) => w.id !== action.payload),
-      };
-
+      return { ...state, workouts: state.workouts.filter((w) => w.id !== action.payload) };
     case "TOGGLE_COMPLETED":
       return {
         ...state,
@@ -20,16 +13,10 @@ const AppReducer = (state, action) => {
           w.id === action.payload ? { ...w, completed: !w.completed } : w
         ),
       };
-
     case "ADD_FAVORITE":
       return { ...state, favorites: [...state.favorites, action.payload] };
-
     case "REMOVE_FAVORITE":
-      return {
-        ...state,
-        favorites: state.favorites.filter((w) => w.id !== action.payload),
-      };
-
+      return { ...state, favorites: state.favorites.filter((w) => w.id !== action.payload) };
     default:
       return state;
   }
