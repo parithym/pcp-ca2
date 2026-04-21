7ec5fcba19220781003c48849fb9fffbd3c8815e
parithym <parithym05@gmail.com>
2026-04-21 09:19:39 +0530

    Implement API token retrieval and data fetching
    Added functions to get token and fetch data from API.

--- api.js
+++ api.js
@@ -1,3 +1,20 @@
+import axios from "axios";
 
+const BASE_URL = "https://t4e-testserver.onrender.com/api";
 
-//export default TrackerCard;
+export const getToken = async () => {
+  const response = await axios.post(`${BASE_URL}/public/token`, {
+    studentId: "E0423017", 
+    password: "451560",        
+  });
+  return response.data.token;
+};
+
+export const getData = async (token) => {
+  const response = await axios.get(`${BASE_URL}/private/data`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data;
+};
